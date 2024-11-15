81e902bc0da5b7b7937785fd246164d736db02ce
Pham Dat <datp3696@gmail.com>
2024-11-15 15:08:27 +0700

    dev commit

--- src/main/java/com/example/Bai1/Bai1Application.java
+++ src/main/java/com/example/Bai1/Bai1Application.java
@@ -1,6 +1,7 @@
 package com.example.Bai1;
 
 import com.example.Bai1.Http.HttpClientTest;
+import com.example.Bai1.Http.HttpUrlTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,18 +16,14 @@ public class Bai1Application {
 	public static void main(String[] args) {
 		try {
 			URL url = new URL("https://jsonplaceholder.typicode.com/albums");
-//            System.out.println(new HttpUrlTest().connect(url));
-			System.out.println(new HttpClientTest().connect(url));
+			System.out.println(new HttpUrlTest().connect(url));
+			//System.out.println(new HttpClientTest().connect(url));
 		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (URISyntaxException e) {
 			throw new RuntimeException(e);
 		} catch (IOException e) {
 			throw new RuntimeException(e);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
 		}
-		SpringApplication.run(Bai1Application.class, args);
+        SpringApplication.run(Bai1Application.class, args);
 	}
 
 }
